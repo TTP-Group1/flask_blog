d11f79d71d71728198dbe12af612161b07dfe663
SQuinones <78192383+SQuinones@users.noreply.github.com>
2022-01-26 18:55:40 -0500

    adding some changes

--- test.py
+++ test.py
@@ -16,10 +16,37 @@ def test2():
     response = app.test_client().get("/register")
     assert response.status_code == 200
     
-
-# def test3():
-#     """A dummy docstring"""
-#     response = app.test_client().get("/edit")
-#     assert b"To Do App" in response.data
-#     assert b"To Do Title" in response.data
-#     assert b"Add" in response.data
+      
+def test3():
+    """Testing Home page """
+    response = app.test_client().get("/")
+    assert b"Puppy Company Blog" in response.data
+    assert b"test" in response.data
+    assert b"brand new title" in response.data
+    assert b"my first six blog post" in response.data
+    assert b"my test post" in response.data
+    assert b"three title" in response.data
+    assert b"asdf" in response.data
+    assert b"Read Blog Post" in response.data
+    
+def test4():
+    """Testing register page"""
+    response = app.test_client().get("/register")
+    assert b"Email" in response.data
+    assert b"Username" in response.data
+    assert b"Password" in response.data
+    assert b"Confirm password" in response.data
+    assert b"Register!" in response.data
+    
+def test5():
+    """Testing Log In page"""
+    response = app.test_client().get("/login")
+    assert b"Email" in response.data
+    assert b"Password" in response.data
+    assert b"Log In!" in response.data
+    
+def test5():
+    """Testing info section """
+    response = app.test_client().get("/info")
+    assert b"Info about our Company" in response.data
+  
